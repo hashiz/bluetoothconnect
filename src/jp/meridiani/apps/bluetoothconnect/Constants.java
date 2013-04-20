23f04f658673c52d4c2c84b6a12b7ef4e810eacb
hashiz <hashiz@meridiani.jp>
2013-04-20 19:44:02 +0900

    * continue

--- src/jp/meridiani/apps/bluetoothconnect/Constants.java
+++ src/jp/meridiani/apps/bluetoothconnect/Constants.java
@@ -3,6 +3,8 @@ package jp.meridiani.apps.bluetoothconnect;
 public class Constants {
 
 	public static final String ACTION_TIMEOUT     = "jp.meridiani.apps.bluetoothconnect.action.TIMEOUT";
+	public static final String BUNDLE_DEVICE_NAME = "jp.meridiani.apps.bluetoothconnect.bundle.DEVICE_NAME";
 	public static final String BUNDLE_DEVICE_ADDR = "jp.meridiani.apps.bluetoothconnect.bundle.DEVICE_ADDR";
-
+	public static final String BUNDLE_CONNECT_AUDIO = "jp.meridiani.apps.bluetoothconnect.bundle.CONNECT_AUDIO";
+	public static final String BUNDLE_CONNECT_HEADSET = "jp.meridiani.apps.bluetoothconnect.bundle.CONNECT_HEADSET";
 }
--- src/jp/meridiani/apps/bluetoothconnect/receiver/FireReceiver.java
+++ src/jp/meridiani/apps/bluetoothconnect/receiver/FireReceiver.java
@@ -1,22 +1,21 @@
 package jp.meridiani.apps.bluetoothconnect.receiver;
 
-import java.util.List;
+import java.util.Set;
 
 import jp.meridiani.apps.bluetoothconnect.Constants;
 import jp.meridiani.apps.bluetoothconnect.R;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiManager;
+import android.net.wifi.WifiInfo;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
@@ -24,11 +23,10 @@ import android.widget.Toast;
 
 public class FireReceiver extends BroadcastReceiver {
 
-	private IntentFilter      mFilter;
-	private boolean           mReset;
-	private BluetoothAdapter mDesireDevice;
+	private IntentFilter     mFilter;
+	private BluetoothDevice  mDesireDevice;
 
-	public class StateReceiver extends BroadcastReceiver {
+	private class StateReceiver extends BroadcastReceiver {
 		public StateReceiver() {
 			Log.d(this.getClass().getName(), "StateReceiver");
 		}
@@ -50,7 +48,6 @@ public class FireReceiver extends BroadcastReceiver {
 
         	    			context.getApplicationContext().unregisterReceiver(this);
         	    			cancelTimer(context);
-        	    			enableNetworks(context);
         	    		}
         	    	}
         	    	break;
@@ -59,6 +56,28 @@ public class FireReceiver extends BroadcastReceiver {
             	}
         	}
         }
+	}
+
+	private class TimeoutReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.d(this.getClass().getName(), "onReceive");
+	    	String action = intent.getAction();
+	    	if (!Constants.ACTION_TIMEOUT.equals(action)) {
+	    		return;
+	    	}
+	    	String deviceName = intent.getStringExtra(Constants.BUNDLE_DEVICE_NAME);
+	    	if (deviceName == null) return;
+	    	String deviceAddr = intent.getStringExtra(Constants.BUNDLE_DEVICE_ADDR);
+	    	if (deviceAddr == null) return;
+	    	boolean connectAudio = intent.getBooleanExtra(Constants.BUNDLE_CONNECT_AUDIO, false);
+	    	if (deviceAddr == null) return;
+	    	boolean connectHeadset = intent.getBooleanExtra(Constants.BUNDLE_CONNECT_HEADSET, false);
+	    	if (deviceAddr == null) return;
+
+	    	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+		}
 	}
 
 	public FireReceiver() {
@@ -78,56 +97,106 @@ public class FireReceiver extends BroadcastReceiver {
         if (bundle == null) {
         	return;
         }
+        String deviceName = bundle.getString(Constants.BUNDLE_DEVICE_NAME);
         String deviceAddr = bundle.getString(Constants.BUNDLE_DEVICE_ADDR);
         if (deviceAddr == null || deviceAddr.length() < 1) {
         	return;
         }
 
-        mReset = false;
+        boolean connectAudio = bundle.getBoolean(Constants.BUNDLE_CONNECT_AUDIO);
+        boolean connectHeadset = bundle.getBoolean(Constants.BUNDLE_CONNECT_HEADSET);
 
-        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-        if (!wifi.isWifiEnabled()) {
-    		Toast.makeText(context, context.getString(R.string.msg_wifi_disable), Toast.LENGTH_LONG).show();
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if (!adapter.isEnabled()) {
+    		Toast.makeText(context, context.getString(R.string.msg_bluetooth_disable), Toast.LENGTH_LONG).show();
     		return;
         }
-        List<WifiConfiguration>wifiList = wifi.getConfiguredNetworks();
-        mDesireWifiConf = null;
-        int lastPriority = 0;
-        for (WifiConfiguration wifiConf : wifiList) {
-        	if (ssid.equals(wifiConf.SSID)) {
-        		if (wifiConf.status == WifiConfiguration.Status.CURRENT) {
-            		Toast.makeText(context, context.getString(R.string.msg_already_connect, ssid), Toast.LENGTH_LONG).show();
-            		return;
-        		}
-        		mDesireWifiConf = wifiConf;
-        	}
-        	if (wifiConf.priority > lastPriority) {
-        		lastPriority = wifiConf.priority;
+        Set<BluetoothDevice>deviceList = adapter.getBondedDevices();
+        mDesireDevice = null;
+        for (BluetoothDevice device : deviceList) {
+        	if (mDesireDevice.getAddress().equals(device.getAddress())) {
+        		mDesireDevice = device;
         	}
         }
-        if (mDesireWifiConf == null) {
-    		Toast.makeText(context, context.getString(R.string.msg_not_configured, ssid), Toast.LENGTH_LONG).show();
+        if (mDesireDevice == null) {
+    		Toast.makeText(context, context.getString(R.string.msg_not_configured, deviceName), Toast.LENGTH_LONG).show();
         	return;
         }
-		Toast.makeText(context, context.getString(R.string.msg_connecting, mDesireWifiConf.SSID), Toast.LENGTH_LONG).show();
+		Toast.makeText(context, context.getString(R.string.msg_connecting, mDesireDevice.getName()), Toast.LENGTH_LONG).show();
 
-		// set priority to top
-		mDesireWifiConf.priority = lastPriority + 1;
-		wifi.updateNetwork(mDesireWifiConf);
-		wifi.saveConfiguration();
+		if (connectAudio) {
+			isConnected(context, adapter, BluetoothProfile.A2DP);
+		}
+		if (connectHeadset) {
+			isConnected(context, adapter, BluetoothProfile.HEADSET);
+		}
 		
-		// disable other networks
-        wifi.enableNetwork(mDesireWifiConf.networkId, true);
-        mReset = true;
-
-		// register
-		context.getApplicationContext().registerReceiver(new StateReceiver(), mFilter);
-
-        wifi.reconnect();
-
         // timer start
         AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         alarm.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 15000, makePendingIntent(context));
+    }
+
+	private static class Listener implements BluetoothProfile.ServiceListener {
+		private BluetoothProfile mProfileProxy = null;
+
+		@Override
+		public void onServiceConnected(int profile, BluetoothProfile proxy) {
+			mProfileProxy = proxy;
+		}
+
+		@Override
+		public void onServiceDisconnected(int profile) {
+			mProfileProxy = null;
+			
+		}
+		public BluetoothProfile getProfileProxy() {
+			return mProfileProxy;
+		}
+		
+	}
+
+	private static BluetoothProfile getProxyProfile(Context context, BluetoothAdapter adapter, int profile) {
+		Listener listener = new Listener();
+    	adapter.getProfileProxy(context, new Listener(), profile);
+
+    	return listener.getProfileProxy() ;
+	}
+
+	private static boolean isConnected(BluetoothProfile proxy, int profile, BluetoothDevice device) {
+
+    	if (proxy == null) {
+			// no present service (has no profile?)
+    		return false;
+		}
+
+    	switch (proxy.getConnectionState(device)) {
+    	case BluetoothProfile.STATE_CONNECTED:
+			// already connected
+			Toast.makeText(context, context.getString(R.string.msg_already_connect, device.getName()), Toast.LENGTH_LONG).show();
+			return true;
+    	case BluetoothProfile.STATE_CONNECTING:
+			// connecting. wait
+			Toast.makeText(context, context.getString(R.string.msg_connecting, device.getName()), Toast.LENGTH_LONG).show();
+			return true;
+		default:
+			return false;
+		}
+    }
+
+	private static void connect(BluetoothProfile proxy, int profile, BluetoothDevice device) {
+
+		// connecting
+		switch (profile) {
+		case BluetoothProfile.A2DP:
+			((BluetoothA2dp)proxy).connect(device);
+			break;
+		case BluetoothProfile.HEADSET:
+			((BluetoothA2dp)proxy).connect(device);
+			break;
+		}
+		
+			Toast.makeText(context, context.getString(R.string.msg_connecting, device.getName()), Toast.LENGTH_LONG).show();
+			return ;
     }
 
     private PendingIntent makePendingIntent(Context context) {
@@ -135,22 +204,10 @@ public class FireReceiver extends BroadcastReceiver {
 	    Intent i = new Intent();
 	    i.setAction(Constants.ACTION_TIMEOUT);
 	    i.setClass(context.getApplicationContext(), TimeoutReceiver.class);
-	    i.putExtra(Constants.BUNDLE_SSID, mDesireWifiConf.SSID);
-	    i.putExtra(Constants.BUNDLE_NETWORKID, mDesireWifiConf.networkId);
+	    i.putExtra(Constants.BUNDLE_DEVICE_NAME, mDesireDevice.getName());
+	    i.putExtra(Constants.BUNDLE_DEVICE_ADDR, mDesireDevice.getAddress());
 	    return PendingIntent.getBroadcast(context.getApplicationContext(), 0, i, 0);
     }
-
-	private void enableNetworks(Context context) {
-		Log.d(this.getClass().getName(), "enableNetworks");
-		if (mReset) {
-	        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-	        List<WifiConfiguration> wifiList = wifi.getConfiguredNetworks();
-	        for (WifiConfiguration wifiConf : wifiList) {
-	       		wifi.enableNetwork(wifiConf.networkId, false);
-	        }
-	        mReset = false;
-		}
-	}
 
 	private void cancelTimer(Context context) {
 		Log.d(this.getClass().getName(), "cancelTimer");
